2c50e9e21fabbf0f32884b185c1216d1757ed4a6
Sergei Litovchenko <serglit72@gmail.com>
2020-02-04 19:21:15 -0800

    Update gitslack.py

--- gitslack.py
+++ gitslack.py
@@ -19,7 +19,7 @@ for i in range(total):
         status = report["workflow_runs"][i]["status"]
         conclusion = report["workflow_runs"][i]["conclusion"]
         timestamp = report["workflow_runs"][i]["created_at"]
-        p_event = "Scheduled on 15:00 UTC run On "+event+" and "+status+" with "+conclusion
+        p_event = "Scheduled on 15:00 UTC run On "+str(event)+" and "+str(status)+" with "+str(conclusion).upper()
         print(p_event)
         break
 
