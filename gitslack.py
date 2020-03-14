1e5148e934d1b681ebc1d241ffeb067729bb3600
Sergei Litovchenko <serglit72@gmail.com>
2020-03-13 20:17:26 -0700

    version changed to 0.7.2 rc1

--- gitslack.py
+++ gitslack.py
@@ -6,7 +6,7 @@ import sys
 web_hook_url = sys.argv[1]
 actions_api = "https://api.github.com/repos/serglit72/azure_queue/actions/runs"
 headers = {'Content-Type':'application/json'}
-message = "BETA v.0.7.1.rc3"
+message = "BETA v.0.7.2.rc1"
 
 get_report = requests.get(url=actions_api,headers=headers)
 report = get_report.json()
